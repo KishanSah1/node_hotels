31cf075c2a8dcb6e321a523345a9092ee9bc9835
Kishan Sah <kishansah7086@gamil.com>
2024-04-20 00:33:04 +0530

    finally working

--- db.js
+++ db.js
@@ -1,14 +1,18 @@
 const mongoose=require('mongoose');
-require('dotenv').config();
+//require('dotenv').config();
 
 
-//const mongoURL='mongodb://127.0.0.1:27017/db';
-const mongoURL=process.env.MONGODB_URL;
-//const mongoURL='mongodb+srv://kishan_sah:123Kishan@cluster0.f6bqvzi.mongodb.net/';
+//const mongoURL='mongodb://127.0.0.1:27017';
+const mongoURL='mongodb+srv://kishanSah:fJdXxFN4aQH6Oca0@cluster0.euvy29a.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 
 mongoose.connect(mongoURL,{
     useNewUrlParser:true,
-    useUnifiedTopology:true
+    useUnifiedTopology:true,
+
+}).then(()=>{
+    console.log('MongoDB server is connected');
+}).catch((err)=>{
+    console.log("no connection",err);
 });
 
 const db=mongoose.connection;
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express = require('express')
-const app = express()
-const db=require('./db')
-require('dotenv').config();
+const express = require('express');
+const app = express();
+const db=require('./db');
+//require('dotenv').config();
 
 const bodyParser=require('body-parser');
 app.use(bodyParser.json());
@@ -18,8 +18,6 @@ app.get('/notes', function (req, res) {
 })
 
 app.use('/person',personRoutes);
-
-const PORT=process.env.PORT
 
 app.listen(3000,()=>{
     console.log('server is running on port 3000');
